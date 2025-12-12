fdfe6bb20a988162c599923dbb6c2b48d0b50e56
Rikin Tuladhar <110732947+RikinTuladhar@users.noreply.github.com>
2025-12-12 17:34:13 +0545

    [Finished: institution panel]

--- Playwright/datas/institution-data.js
+++ Playwright/datas/institution-data.js
@@ -3,4 +3,112 @@ export const institutionData = {
   email: "testing.automation.playwright@gmail.com",
   password: "testing.automation.playwright@gmail.com",
   confirm_password: "testing.automation.playwright@gmail.com",
+  country: "Nepal",
+  state: "Bagmati",
+  city: "Kathmandu",
+  street: "Kalimati 32",
+  location_map_embed: "<iframe src=''></iframe>",
+  video_url:
+    "https://advisebridge.com/institution/institutions/testing-university?step=information&tab=-information-tab",
+
+  application_portal_url:
+    "https://advisebridge.com/institution/institutions/testing-university?step=information&tab=-information-tab",
+
+  total_students: "1000",
+  international_students: "1000",
+  funding_type: "Private",
+  institution_type: "College",
+  established_year: "2020",
+  notes: "Testing notes",
+  institution_description: "Testing institution description",
+  institution_glance: "Testing glance",
+  institution_overview: "Testing overview",
+  eligibilities: [
+    {
+      selected_education_level: "Bachelor's Degree",
+      required_education_level: "High School",
+      gpa_total: "4",
+      toefl: "120",
+      ielts: "8",
+      pte: "10",
+      duolingo: "10",
+      gre: "320",
+      gmat: "100",
+      sat: "1200",
+    },
+    {
+      selected_education_level: "Master's Degree",
+      required_education_level: "Bachelor's Degree",
+      gpa_total: "4",
+      toefl: "120",
+      ielts: "8",
+      pte: "10",
+      duolingo: "10",
+      gre: "320",
+      gmat: "100",
+      sat: "1200",
+    },
+  ],
+  education_level: [
+    {
+      education_level: "Bachelor's Degree",
+      tution_fee: "12000",
+      commission_type: "Flat",
+      commission_tution_fee: "1000",
+      commission_of_agent: "1200",
+      application_fee_strikthrough: "12000",
+      application_fee: "100",
+      deposit_amount: "1200",
+      accommodation_other_fee: "1200",
+      majors: [
+        {
+          id: 1,
+          name: "Artificial Intelligence",
+        },
+        {
+          id: 2,
+          name: "Accounting",
+        },
+        {
+          id: 3,
+          name: "Agriculture",
+        },
+      ],
+    },
+    {
+      education_level: "Master's Degree",
+      tution_fee: "12000",
+      commission_type: "Flat",
+      commission_tution_fee: "1000",
+      commission_of_agent: "1200",
+      application_fee_strikthrough: "12000",
+      application_fee: "100",
+      deposit_amount: "1200",
+      accommodation_other_fee: "1200",
+      majors: [
+        {
+          id: 1,
+          name: "Artificial Intelligence",
+        },
+        {
+          id: 2,
+          name: "Accounting",
+        },
+        {
+          id: 3,
+          name: "Agriculture",
+        },
+      ],
+    },
+  ],
+  scholarship: [
+    {
+      title: "This is first scholarship",
+      description: "This is first scholarship description",
+    },
+    {
+      title: "This is second scholarship",
+      description: "This is second scholarship description",
+    },
+  ],
 };
