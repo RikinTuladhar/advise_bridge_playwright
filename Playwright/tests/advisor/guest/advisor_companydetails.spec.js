793593a268ef0cc9f4101343af8b558c7c6926a3
RabinaChalaune <chalaunrrabina@gmail.com>
2026-01-05 17:29:27 +0545

    done

--- Playwright/tests/advisor/guest/advisor_companydetails.spec.js
+++ Playwright/tests/advisor/guest/advisor_companydetails.spec.js
@@ -8,11 +8,20 @@ test("Advisor company details page", async ({ page }) => {
   await expect(page).toHaveURL('https://advisebridge.com/advisor/company-details');
 
  
-  await expect(page.getByText('Company Name')).toBeVisible();
-  await expect(page.getByText('Owner Name')).toBeVisible();
-  await expect(page.getByText('Established')).toBeVisible();
-  await expect(page.getByText('Country')).toBeVisible();
-  await expect(page.getByText('Address')).toBeVisible();
-  await expect(page.getByText('Phone')).toBeVisible();
-  await expect(page.getByText('Mobile')).toBeVisible();
+  await expect(page.getByText('Company Name')).toBeVisible({ timeout: 5000 });
+  await expect(page.getByText('Owner Name')).toBeVisible({ timeout: 5000 });
+  await expect(page.getByText('Established')).toBeVisible({ timeout: 5000 });
+  await expect(page.getByText('Country')).toBeVisible({ timeout: 5000 });
+  await expect(page.getByText('Address')).toBeVisible({ timeout: 5000 });
+  await expect(page.getByText('Phone')).toBeVisible({ timeout: 5000 });
+  await expect(page.getByText('Mobile')).toBeVisible({ timeout: 5000 });
+
+  await page.getByRole("button", { name: "Save & Continue" }).click();
+
+  await expect(page.getByText('Contact person')).toBeVisible({ timeout: 5000 });
+  await expect(page.getByText('Contact email')).toBeVisible({ timeout: 5000 });
+  await expect(page.getByText('Contact phone')).toBeVisible({ timeout: 5000 });
+
+  await page.getByRole("button", { name: "Save & Continue" }).click();
+
 });
