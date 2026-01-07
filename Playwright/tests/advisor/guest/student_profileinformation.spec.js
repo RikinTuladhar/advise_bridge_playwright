bed38b0a90db7f5e333d6b0e9d19a1cefc8bcbe0
Mamata Khanal <160756488+mamatakhanal@users.noreply.github.com>
2026-01-07 17:21:30 +0545

    add new

--- Playwright/tests/advisor/guest/student_profileinformation.spec.js
+++ Playwright/tests/advisor/guest/student_profileinformation.spec.js
@@ -10,22 +10,20 @@ test("Student Dashboard", async ({ page }) => {
     await page.waitForTimeout(3000);
 
     //Profile Information
-    await page.fill("#data\\.first_name", "mamata");
+    await page.fill("#data\\.first_name", "Akshata");
     await page.waitForTimeout(2000);
-    await page.fill("#data\\.last_name", "khanal");
+    await page.fill("#data\\.last_name", "nepal");
     await page.waitForTimeout(2000);
     await page.fill("#data\\.phone", "9744229321");
     await page.waitForTimeout(2000);
 
-    /*
-    await page.locator('#data\\.dob');
-    await page.locator('input[x-model.debounce="focusedYear"]').fill('2005'); //Year
-    await page.waitForTimeout(2000);
-    await page.locator('select[x-model="focusedMonth"]').selectOption('5');  //Month
-    await page.waitForTimeout(2000);
-    await page.locator('[role="option"]', { hasText: '15' }).click(); //Day
-    await page.waitForTimeout(2000);
-    */
+    // await page.locator('#data\\.dob');
+    // await page.locator('input[x-model.debounce="focusedYear"]').fill('2005'); //Year
+    // await page.waitForTimeout(2000);
+    // await page.locator('select[x-model="focusedMonth"]').selectOption('5');  //Month
+    // await page.waitForTimeout(2000);
+    // await page.locator('[role="option"]', { hasText: '15' }).click(); //Day
+    // await page.waitForTimeout(2000);
 
     await page.locator('input[name="data.gender"][value="female"]').check();
     await page.waitForTimeout(2000);
@@ -35,86 +33,108 @@ test("Student Dashboard", async ({ page }) => {
     await page.waitForTimeout(5000);
 
 
+    //Address
+    // await page.click("//button[normalize-space()='Address']");
+    // await expect(page).toHaveURL("https://www.advisebridge.com/student/students/1478?tab=-address-tab");
+    // await page.waitForTimeout(3000);
+    // await page.locator('.choices__inner').click();
+    // await page.locator('.choices__list--dropdown .choices__item', {
+    //     hasText: 'Afghanistan'
+    // }).click();
+
+
+
     //Language
     await page.click("//button[normalize-space()='Language']");
     await expect(page).toHaveURL("https://www.advisebridge.com/student/students/1478?tab=-language-tab");
-    await page.waitForTimeout(3000);
-
+    await page.waitForTimeout(2000);
     await page.locator('.choices__inner').click();
     await page.locator('.choices__list .choices__item', { hasText: 'IELTS' }).click();
     await expect(page.locator('.choices__item--selectable.is-selected')).toHaveText('IELTS');
-    await page.waitForTimeout(2000);
+    await page.waitForTimeout(1000);
     await page.fill('#data\\.speaking_score', '7');
-    await page.waitForTimeout(2000);
+    await page.waitForTimeout(1000);
     await page.fill('#data\\.reading_score', '7');
-    await page.waitForTimeout(2000);
+    await page.waitForTimeout(1000);
     await page.fill('#data\\.writing_score', '7');
-    await page.waitForTimeout(2000);
+    await page.waitForTimeout(1000);
     await page.fill('#data\\.listening_score', '7');
-    await page.waitForTimeout(2000);
+    await page.waitForTimeout(1000);
     await page.fill('#data\\.average_score', '7');
-    await page.waitForTimeout(2000);
-
+    await page.waitForTimeout(1000);
     await page.locator('button:has-text("Save")').nth(2).click();
+    await page.waitForTimeout(5000);
 
 
-await saveBtn.click();
-
-    // Click the button that contains a span with text "Save"
-    // await page.locator('button:has-text("Save")').nth(0).click();
-
-//await page.click("//button[.//span[normalize-space()='Save']]");
-//await page.waitForTimeout(5000);
-    // Locate the button containing the span with text "Save" and click it
-    //await page.locator('button', { hasText: 'Save' }).click();
+    //GPA & KSE
+    await page.click("//button[normalize-space()='GPA & KSE']");
+    await expect(page).toHaveURL("https://www.advisebridge.com/student/students/1478?tab=-gpa-kse-tab");
+    await page.waitForTimeout(1000);
+    await page.locator('.choices__inner', { has: page.locator('#data\\.gpa_id') }).click();
+    await page.locator('.choices__list--dropdown .choices__item', { hasText: '0-4' }).click();
+    await page.waitForTimeout(1000);
 
 
-    // 1️⃣ Click the date input to open the picker
-    //await page.locator('#data\\.exam_date').click();
+    // Open GPA dropdown
+    await page
+        .locator('.choices:has(#data\\.gpa_score_id) .choices__inner')
+        .click();
 
-    // await page.click("//button[normalize-space()='Save']");
-    // await page.waitForTimeout(5000);
+    // Type GPA in search box
+    await page
+        .locator('.choices:has(#data\\.gpa_score_id) input.choices__input--cloned')
+        .fill('1.75');
 
-    // Suppose your form has id "language-score-form"
-    //const saveButton = page.locator('#language-score-form button', { hasText: 'Save' });
-    //await saveButton.click();
+    // Click GPA value
+    await page
+        .locator(
+            '.choices:has(#data\\.gpa_score_id) .choices__list--dropdown .choices__item',
+            { hasText: '1.75' }
+        )
+        .click();
 
 
-    // Click the button that contains a span with text 'Save'
-    //await page.click("//button[.//span[normalize-space()='Save']]");
+    // // Scope to GPA score dropdown
+    // const gpaScore = page.locator('.choices', {
+    //     has: page.locator('#data\\.gpa_score_id'),
+    // });
+
+    // // Open dropdown
+    // await gpaScore.locator('.choices__inner').click();
+
+    // // Wait until THIS dropdown is active
+    // await expect(
+    //     gpaScore.locator('.choices__list--dropdown')
+    // ).toHaveClass(/is-active/);
+
+    // // Type into search box
+    // await gpaScore
+    //     .locator('input.choices__input--cloned')
+    //     .fill('1.75');
+
+    // // Click the score
+    // await gpaScore
+    //     .locator('.choices__list--dropdown .choices__item', { hasText: '1.75' })
+    //     .click();
 
 
+
+
+    await page.locator('button:has-text("Save")').nth(3).click();
+    await page.waitForTimeout(5000);
 
 });
 
 /*
-   
-//Address
-    await page.click("//button[normalize-space()='Address']");
+    //Academics
+    await page.click("//button[normalize-space()='Academics']");
     await expect(page).toHaveURL("https://www.advisebridge.com/student/students/1478?tab=-address-tab");
-    await page.waitForTimeout(3000);
+    await page.waitForTimeout(5000);
 
-   await page.locator('.choices__inner').click();
-   await page.locator('.choices__list--dropdown .choices__item', {
-       hasText: 'Afghanistan'
-   }).click();
-
-   */
-
-/*
-  //GPA & KSE
-  await page.click("//button[normalize-space()='GPA & KSE']");
-  await expect(page).toHaveURL("https://www.advisebridge.com/student/students/1478?tab=-address-tab");
-  await page.waitForTimeout(5000);
- 
-  //Academics
-  await page.click("//button[normalize-space()='Academics']");
-  await expect(page).toHaveURL("https://www.advisebridge.com/student/students/1478?tab=-address-tab");
-  await page.waitForTimeout(5000);
- 
-  //Documents
-  await page.click("//button[normalize-space()='Documents']");
-  await expect(page).toHaveURL("https://www.advisebridge.com/student/students/1478?tab=-address-tab");
-  await page.waitForTimeout(5000);
+    //Documents
+    await page.click("//button[normalize-space()='Documents']");
+    await expect(page).toHaveURL("https://www.advisebridge.com/student/students/1478?tab=-address-tab");
+    await page.waitForTimeout(5000);
  
 */
+
