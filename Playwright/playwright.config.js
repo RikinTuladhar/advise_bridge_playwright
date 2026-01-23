f3e240afd7cd2b3cd0bc67d159ee7172e7edea30
Rikin Tuladhar <110732947+RikinTuladhar@users.noreply.github.com>
2026-01-23 11:11:05 +0545

    [Add] Staging dialog bypass

--- Playwright/playwright.config.js
+++ Playwright/playwright.config.js
@@ -15,11 +15,15 @@ export default defineConfig({
   retries: process.env.CI ? 2 : 0,
   workers: process.env.CI ? 1 : undefined,
   reporter: "html",
-  
+
   use: {
     // 👇 Set default base URL
     baseURL: "https://advisebridge.com",
     trace: "on-first-retry",
+    httpCredentials: {
+      username: "advisebridge",
+      password: "advisebridge",
+    },
   },
 
   projects: [
