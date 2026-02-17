fd5d86fedeaada1eee2a688fa83af6bcf143ed43
RabinaChalaune <chalaunrrabina@gmail.com>
2026-02-17 13:20:41 +0545

    Done

--- pages/advisor/advisor_login.js
+++ pages/advisor/advisor_login.js
@@ -1,15 +1,35 @@
-import { expect } from "@playwright/test";
+// import { expect } from "@playwright/test";
 
-export class AdvisorLoginPage {
+// export class AdvisorLoginPage {
+//   constructor(page) {
+//     this.page = page;
+//     this.emailInput = page.locator("#email");
+//     this.passwordInput = page.locator("#password");
+//     this.loginButton = page.locator("//button[normalize-space()='Log in as advisor']");
+
+// }
+//   async goToAdvisorLoginGuest() {
+//     await this.page.goto("/login?tab=advisor");
+//   }
+//   async logInAsGuestToAdvisor(email, password) {
+//     await this.emailInput.fill(email)
+//     await this.passwordInput.fill(password);
+//     await this.loginButton.click();
+//   }
+//   async sucessfullLogin() {
+//     await expect(this.page).toHaveURL("/advisor");
+//   }
+// }
+import { expect } from "@playwright/test";
+ export class AdvisorLoginPage{
   constructor(page) {
     this.page = page;
     this.emailInput = page.locator("#email");
     this.passwordInput = page.locator("#password");
-    this.loginButton = page.locator("//button[normalize-space()='Log in as advisor']");
-
-}
-  async goToAdvisorLoginGuest() {
-    await this.page.goto("/login?tab=advisor");
+    this.loginButton = page.locator("button[normalize-space()='log in as advisor']");
+  }
+  async goToAdvisorLoginGuest(){
+    await this.page.goto("/Login?tab=advisor")
   }
   async logInAsGuestToAdvisor(email, password) {
     await this.emailInput.fill(email)
@@ -19,4 +39,4 @@ export class AdvisorLoginPage {
   async sucessfullLogin() {
     await expect(this.page).toHaveURL("/advisor");
   }
-}
+ }
--- tests/advisor_module/login/advisor_login.spec.js
+++ tests/advisor_module/login/advisor_login.spec.js
@@ -1,31 +1,45 @@
-import { test } from "@playwright/test";
-import { AdvisorLoginPage } from "../../../pages/advisor/advisor_login.js";
+// import { test } from "@playwright/test";
+// import { AdvisorLoginPage } from "../../../pages/advisor/advisor_login.js";
+// import { advisor_login } from "../../../datas/advisor_data.js";
+
+// test("Valid Login", async ({ page }) => {
+//     const advisorPage = new AdvisorLoginPage(page);
+//   await advisorPage.goToAdvisorLoginGuest();
+//   await advisorPage.logInAsGuestToAdvisor(advisor_login.email, advisor_login.password);
+//   await advisorPage.sucessfullLogin();
+// });
+
+
+// // test("Invalid email", async ({ page }) => {
+// //   const advisor = {
+// //     email: "invalid@gmail.com",
+// //     password: "bestNepal@123",
+// //   };
+// //   const advisor_page = new AdvisorPage(page);
+// //   await advisor_page.goToAdvisorLoginGuest();
+// //   await advisor_page.logInAsGuestToAdvisorInvalidEmail(advisor.email, advisor.password);
+// // });
+
+// // test("Invalid password", async ({ page }) => {
+// //   const advisor = {
+// //     email: "invalid@gmail.com",
+// //     password: "bestNepal@123",
+// //   };
+// //   const advisor_page = new AdvisorPage(page);
+// //   await advisor_page.goToAdvisorLoginGuest();
+// //   await advisor_page.logInAsGuestToAdvisorInvalidPassword(advisor.email, advisor.password);
+// // });
+
+import { test, expect} from '@playwright/test';
+import { AdvisorLoginPage } from '../../../pages/advisor/advisor_login';
 import { advisor_login } from "../../../datas/advisor_data.js";
 
-test("Valid Login", async ({ page }) => {
-    const advisorPage = new AdvisorLoginPage(page);
-  await advisorPage.goToAdvisorLoginGuest();
-  await advisorPage.logInAsGuestToAdvisor(advisor_login.email, advisor_login.password);
-  await advisorPage.sucessfullLogin();
-});
+test('test', async ({ page })=> {
+// Login
+const AdvisorPage=new AdvisorLoginPage(page);
+await AdvisorPage.goToAdvisorLoginGuest();
+await AdvisorPage.logInAsGuestToAdvisor(advisor_login.email, advisor_login.password)
+await AdvisorPage.sucessfullLogin();
+await page.waitForTimeout(2000);
 
-
-// test("Invalid email", async ({ page }) => {
-//   const advisor = {
-//     email: "invalid@gmail.com",
-//     password: "bestNepal@123",
-//   };
-//   const advisor_page = new AdvisorPage(page);
-//   await advisor_page.goToAdvisorLoginGuest();
-//   await advisor_page.logInAsGuestToAdvisorInvalidEmail(advisor.email, advisor.password);
-// });
-
-// test("Invalid password", async ({ page }) => {
-//   const advisor = {
-//     email: "invalid@gmail.com",
-//     password: "bestNepal@123",
-//   };
-//   const advisor_page = new AdvisorPage(page);
-//   await advisor_page.goToAdvisorLoginGuest();
-//   await advisor_page.logInAsGuestToAdvisorInvalidPassword(advisor.email, advisor.password);
-// });
+})
